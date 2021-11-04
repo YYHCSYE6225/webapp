3af80168792db731a7e33886e8a24b81e815a0e9
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-11-04 13:20:18 -0400

    cicd testing

--- src/main/java/edu/neu/coe/csye6225/webapp/controller/NotesController.java
+++ src/main/java/edu/neu/coe/csye6225/webapp/controller/NotesController.java
@@ -13,6 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 public class NotesController {
     @GetMapping(value = "")
     public String getUser(){
-        return "test cicd";
+        return "cicd testing";
     }
 }
