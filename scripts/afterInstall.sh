9cb972b30637cd5f897c7523e8cea36316642862
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-11-11 17:38:01 -0500

    cloud

---
+++ scripts/afterInstall.sh
@@ -0,0 +1,6 @@
+sudo cp /var/webapp/scripts/cloudwatch-config.json /opt/
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
