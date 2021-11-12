e3e72fdbe8c3b282d0e4f44d1f4824ed3d18b5b6
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-11-11 20:52:08 -0500

    cloud

--- src/main/java/edu/neu/coe/csye6225/webapp/controller/UserController.java
+++ src/main/java/edu/neu/coe/csye6225/webapp/controller/UserController.java
@@ -67,7 +67,7 @@ public class UserController {
     @GetMapping(value = "/self")
     public ResponseEntity<User> getUser(HttpServletRequest request){
         User user=userService.getUserSelf(request);
-        logger.info(new ResponseEntity(user,HttpStatus.OK).toString());
+        logger.warn(new ResponseEntity(user,HttpStatus.OK).toString());
         return new ResponseEntity(user,HttpStatus.OK);
     }
 
