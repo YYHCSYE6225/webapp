b9b72122db972828d130d0044cf234bfcb90225d
YYhyang <44155580+YYhyang@users.noreply.github.com>
2021-12-08 00:44:10 -0500

    ssh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,2 +1,2 @@
-kill -9 $(netstat -nlp | grep :80 | awk '{print $7}' | awk -F"/" '{ print $1 }')
+kill -9 $(netstat -nlp | grep :443 | awk '{print $7}' | awk -F"/" '{ print $1 }')
 sudo rm -rf /var/webapp
